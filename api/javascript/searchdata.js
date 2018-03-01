def70abdc47411094e080cf0696db7e7c3b96364
FascinatedBox <FascinatedBox@gmail.com>
2018-03-01 13:37:09 -0800

    Update for coroutine tutorial.

--- api/javascript/searchdata.js
+++ api/javascript/searchdata.js
@@ -1,5 +1,5 @@
 var indexSectionsWithContent = {
-   "Macros": {
+   "Classes": {
       "Symbols": false,
       "Numbers": false,
       "A": false,
@@ -29,7 +29,7 @@ var indexSectionsWithContent = {
       "Y": false,
       "Z": false
       },
-   "Types": {
+   "Functions": {
       "Symbols": false,
       "Numbers": false,
       "A": false,
@@ -89,7 +89,7 @@ var indexSectionsWithContent = {
       "Y": false,
       "Z": false
       },
-   "Classes": {
+   "Types": {
       "Symbols": false,
       "Numbers": false,
       "A": false,
@@ -119,7 +119,7 @@ var indexSectionsWithContent = {
       "Y": false,
       "Z": false
       },
-   "Functions": {
+   "Macros": {
       "Symbols": false,
       "Numbers": false,
       "A": false,
